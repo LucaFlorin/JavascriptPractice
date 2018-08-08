c77ecc159bec95de33ac36aaeb5b90b27056dc40
LucaFlorin <luca.florin.adrian@gmail.com>
2018-08-08 14:16:11 +0300

    Javascript Practice File

---
+++ index1.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+        window.alert(myFunction);
+}
+
+
+myFunction();
+
+
+function myFunction1() {
+    document.getElementById("demo3").innerHTML = "Hello Dolly!";
+    document.getElementById("demo4").innerHTML = "How are you?";
+}
+
+myFunction1();
